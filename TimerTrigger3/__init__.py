0c8dab0b00ad6869242da95503b930741b59f800
Josh McCurry <joshmccurry@microsoft.com>
2023-02-27 08:45:29 -0500

    Missing 3rd Trigger

---
+++ TimerTrigger3/__init__.py
@@ -0,0 +1,26 @@
+import datetime
+import logging
+import os
+import asyncio
+
+import azure.functions as func
+from azure.identity import ManagedIdentityCredential
+from azure.cosmos import CosmosClient
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+        
+
+    identity = os.environ["identity"]
+    cosmos = os.environ["cosmos_url"]
+    mi_credential = ManagedIdentityCredential(client_id=identity)
+    CosmosClient(cosmos, mi_credential,
+                        user_agent="AzureFunction",
+                        user_agent_overwrite=True,
+                        connection_verify=True, 
+                        consistency_level="Session")
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
